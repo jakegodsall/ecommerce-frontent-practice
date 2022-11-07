373b7e562d582457aa6fe359c690e7ff180204d0
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-07 19:46:52 +0000

    added overlay with basic styling and toggling with hamburger

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,8 +1,12 @@
 const hamburgerButton = document.getElementById('hamburger');
 const innerHamburgerButton = document.getElementById('inner-hamburger');
+const overlay = document.getElementById('overlay');
+
+console.log(overlay);
 
 const hamburgerHandler = () => {
     hamburgerButton.classList.toggle('opened');
+    overlay.classList.toggle('active');
 };
 
 hamburgerButton.addEventListener('click', hamburgerHandler);
